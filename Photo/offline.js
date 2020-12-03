1bce9c8894ccff06fcf73155a5c0e069283d90e8
Night İnc <75408420+NightPhoto@users.noreply.github.com>
2020-12-03 07:48:04 +0300

    Add files via upload

---
+++ Photo/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1606970751,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"bash/sprite2-sheet0.png",
+		"bash/2d_starfield.png",
+		"bash/ımglog-sheet0.png",
+		"bash/sizebox-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"button.css",
+		"a.png"
+	]
+}
